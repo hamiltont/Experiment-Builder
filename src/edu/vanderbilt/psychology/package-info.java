e723346a76b08604c8eb045ecaeca5f48aa7d22e
Hamilton Turner <hamiltont@gmail.com>
2010-12-27 23:26:30 -0500

    Started the basic player

--- src/edu/vanderbilt/psychology/package-info.java
+++ src/edu/vanderbilt/psychology/package-info.java
@@ -1,6 +1,10 @@
 /**
  * Contains a standard Model-View-Controller pattern. 
  * 
+ * Currently contains code for both the builder and the player 
+ * component. Eventually should be factored to contain an inner 
+ * 'builder' and a 'player' package 
+ * 
  * @author Hamilton Turner
  */
 package edu.vanderbilt.psychology;
---
+++ src/edu/vanderbilt/psychology/player/Player.java
@@ -0,0 +1,33 @@
+package edu.vanderbilt.psychology.player;
+
+import javax.swing.JFrame;
+
+/**
+ * The entry point for the experiment player
+ * 
+ * @author hamiltont
+ * 
+ */
+public class Player {
+
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				// Make sure we have nice window decorations.
+				JFrame.setDefaultLookAndFeelDecorated(true);
+
+				// Create and set up the window.
+				JFrame frame = new JFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setResizable(false);
+
+				// Display the window.
+				frame.setVisible(true);
+			}
+		});
+
+	}
+
+}
